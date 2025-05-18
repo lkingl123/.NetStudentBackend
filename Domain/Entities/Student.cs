363cd80f193c6f3523bc58d3ae1381350534b6d3
lkingl123 <lokekingloong@gmail.com>
2025-05-18 14:41:59 -0600

    Add project files.

---
+++ Domain/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities
+{
+    public class Student
+
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+    
+
+        public Student(string firstName, string lastName, string? email)
+        {
+            FirstName = firstName;
+            LastName = lastName;    
+            Email = email;
+        }
+    }
+}
---
+++ Domain/Interfaces/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task<Student> GetStudentByIdAsync(string id);
+        Task<IEnumerable<Student>> GetAllStudentsAsync();
+    }
+}
